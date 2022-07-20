42c31f44f8b726f20da2d610102fb0142de76d80
jasarmstrong32 <jasmine.armstrong515@gmail.com>
2022-07-20 17:35:18 -0500

    2 files modified

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 const noImgFound = "https://th.bing.com/th/id/OIP.ke9cNKXh8oX10dfQiEryKgAAAA?w=148&h=180&c=7&r=0&o=5&pid=1.7";
 
-const movieSearchBox = document.getElementById('movie-search-box');
+const movieSearchBox = document.getElementById('movie-search-box').addEventListener('input', findMovies);
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
+let moviePoster= '';
 // const button = document.querySelector('button');
 
 // button.addEventListener('click', findMovies());
@@ -13,12 +14,13 @@ async function loadMovies(searchTerm) {
   const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=${apiKey}`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
-  // console.log(data.Search);
+  console.log(data.Search);
   if ((data.Response = 'true')) displayMovieList(data.Search);
 }
 
 function findMovies() {
-  let searchTerm = movieSearchBox.value.trim();
+  let searchTerm =  document.getElementById('movie-search-box').value.trim();
+  console.log(searchTerm);
   if (searchTerm.length > 0) {
     searchList.classList.remove('hide-search-list');
     loadMovies(searchTerm);
@@ -29,8 +31,6 @@ function findMovies() {
 
 function displayMovieList(movies) {
   searchList.innerHTML = '';
-  for (let i = 0; i < movies.length; i++) {
-    let movieListItem = document.createElement('div');
     for(let i = 0; i < movies.length; i++){
       let movieListItem = document.createElement('div');
       movieListItem.dataset.id = movies[i].imdbID;
@@ -61,7 +61,7 @@ function displayMovieList(movies) {
     searchListMovies.forEach(movie => {
       movie.addEventListener('click', async () => {
         searchList.classList.add('hide-search-list');
-        movieSearchBox.value = "";
+        // movieSearchBox.value = "";
         const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=${apiKey}`);
         const movieDetails = await result.json();
 
@@ -69,7 +69,6 @@ function displayMovieList(movies) {
       });
     });
   }
-}
 
 function displayMovieDetails(details){
   resultGrid.innerHTML = `
